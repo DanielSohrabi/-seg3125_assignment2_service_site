64c00212ed0e31b7a323578b8de60aa1f06b48e8
Daniel Sohrabi <daniel.sohrabi.2005@gmail.com>
2025-06-08 18:14:00 -0400

    Book an appointment page complete styling

--- service_site/src/pages/Appointments.jsx
+++ service_site/src/pages/Appointments.jsx
@@ -4,20 +4,27 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import './Appointments.css'
+
 export default function Appointments(){
     return (
         <>
         
-            <Container className="text-center">
+            <Container className="text-center py-5">
                 <h1>Book an Appointment</h1>
             </Container>
-            <Container className="home-introduction">
+            <Container className="">
                 <Row>
-                    <Col xs={6}>
+                    <Col xs={6} className="square-form">
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicName">
-                                <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="email" placeholder="Enter full name" />
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="email" placeholder="Enter first name" />
+                            </Form.Group>
+                        
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="email" placeholder="Enter last name" />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -48,13 +55,13 @@ export default function Appointments(){
                             </Form.Group>
 
 
-                            <Button variant="primary" type="submit">
+                            <Button className='hero-button' variant="primary" type="submit">
                                 Submit
                             </Button>
                         </Form>
                     </Col>
                     <Col xs={6}>
-                        <Row>
+                        <Row className="contact-info">
                             <Col>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                                     <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
@@ -62,12 +69,12 @@ export default function Appointments(){
                                 </svg>
                             </Col>
                             <Col>
-                                <p>**************</p>
+                                <p>****************************</p>
                             </Col>
 
                         </Row>
                         
-                        <Row>
+                        <Row className="contact-info">
                             <Col>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
@@ -75,11 +82,11 @@ export default function Appointments(){
                             </Col>
 
                             <Col>
-                                <p>**************</p>
+                                <p>****************************</p>
                             </Col>
                         </Row>
                         
-                        <Row>
+                        <Row className="contact-info">
                             <Col>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
@@ -87,16 +94,18 @@ export default function Appointments(){
                             </Col>
 
                             <Col>
-                                <p>**************</p>
+                                <p>****************************</p>
                             </Col>
                         </Row>
 
-                        <Row>
+                        <Row className="hours-operation">
                             <h2>Hours of Operation</h2>
-                            <li>Something</li>
-                            <li>Something</li>
-                            <li>Something</li>
-                            <li>Something</li>
+                            <Container className="px-5">
+                                <li>Mon-Thurs                                                                   XXX</li>
+                                <li>Friday                                                                          XXX</li>
+                                <li>Saturday                                                                     XXX</li>
+                                <li>Sunday                                                                        XXX</li>
+                            </Container>
                         </Row>
 
                     </Col>
