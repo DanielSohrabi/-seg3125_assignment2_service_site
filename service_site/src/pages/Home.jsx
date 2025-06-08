1c824bbc21f585883e3322930b2a82788e038a3a
Daniel Sohrabi <daniel.sohrabi.2005@gmail.com>
2025-06-08 14:28:28 -0400

    Home Page Placeholder

---
+++ service_site/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+// import './App.css'
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+function Home() {
+  return (
+    <>
+      <div>
+        <Container fluid>
+
+          <h1 className='hero-button'>Heading Text</h1>
+          <Button className='view-services'>View Services</Button>
+          <Image src="../assets/pexels-luftschnitzel-100582.jpg" fluid></Image>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default Home
